6ca00083b89fa58dba8a42c3765532753cdf2c1f
Lukshana <154061n@uom.lk>
2022-02-23 18:32:13 +0530

    2nd commit

---
+++ frontpage/backendpage/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const signUpTempletecopy = require('../models/SignUpModels')
+
+router.post('/signup', (request, response) =>{
+    const signedUpUser = new signUpTempletecopy({
+        fullName:request.body.fullName,
+        username:request.body.username,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signedUpUser.save()
+    .then(data =>{
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
+})
+router.get('/signin')
+module.exports = router
